f39555fdf8e2b2a098163b8b5aa90fc71caddc1b
Jamie Naioti <naioti@gmail.com>
2016-01-14 10:38:44 -0600

    first commit - adding at the point where the todo has items and can add a new task. Starting on Projects list

---
+++ www/js/app.js
@@ -0,0 +1,62 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('todo', ['ionic'])
+
+.controller('TodoCtrl', function ($scope, $ionicModal) {
+
+  $scope.tasks = [
+    {title: 'Collect coins'},
+    {title: 'Eat mushrooms'},
+    {title: 'Ge high enough to grab the flag'},
+    {title: 'Find the Princess'}
+  ];
+
+  // Create and load the modal
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  // Called when the form is submitted
+  $scope.createTask = function(task) {
+    $scope.tasks.push({
+      title: task.title
+    });
+    $scope.taskModal.hide();
+    task.title = "";
+  };
+
+  // Open our new task Modal
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  };
+
+  // Close our new task Modal
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  }
+  
+});
+
+// .run(function($ionicPlatform) {
+//   $ionicPlatform.ready(function() {
+//     if(window.cordova && window.cordova.plugins.Keyboard) {
+//       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+//       // for form inputs)
+//       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+//       // Don't remove this line unless you know what you are doing. It stops the viewport
+//       // from snapping when text inputs are focused. Ionic handles this internally for
+//       // a much nicer keyboard experience.
+//       cordova.plugins.Keyboard.disableScroll(true);
+//     }
+//     if(window.StatusBar) {
+//       StatusBar.styleDefault();
+//     }
+//   });
+// })
